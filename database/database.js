6f21cd7982efba966e61ac5ca0531263e64ff69f
Isadora Vasconcellos <isadora.vasconcellos@gmail.com>
2022-07-26 00:01:47 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -5,10 +5,16 @@ const DATABASE= process.env.DATABASE;
 const USER_DATABASE = process.env.USER_DATABASE;
 const PASSWORD_DATABASE = process.env.PASSWORD_DATABASE;
 
-const sequelize = new Sequelize(DATABASE, USER_DATABASE, PASSWORD_DATABASE, {
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // localhost
+    sequelize = new Sequelize(DATABASE, USER_DATABASE, PASSWORD_DATABASE, {
     host: "localhost",
     dialect: "mysql",
     timezone: "-03:00"
-});
+    });
+}
 
 module.exports = sequelize;
